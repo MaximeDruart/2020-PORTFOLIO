91dbfe4c5a2635ac11604c2cb4d48b7421bf34ae
Maxime Druart <maxime41010.druart@gmail.com>
2020-03-16 13:25:07 +0100

    using hash router instead of router for gh pages

--- src/AppWrap.js
+++ src/AppWrap.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useContext } from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, HashRouter, Route, Switch } from "react-router-dom"
 import projectData from "./assets/projectData"
 import Home from "./components/Home"
 import About from "./components/About"
@@ -28,7 +28,7 @@ const AppWrap = () => {
   return (
     <div className="wrapper">
       <div ref={$transitionHack} className="transition-hack"></div>
-      <Router>
+      <HashRouter basename="/">
         <MouseFollower />
         {!context.removeLoader ? <Loader /> : <div className="noise-filter"></div>}
         <Route path="/" render={props => <Header {...props} />} />
@@ -37,7 +37,7 @@ const AppWrap = () => {
           <Route path="/about" exact component={About} />
           {projectRoutes}
         </Switch>
-      </Router>
+      </HashRouter>
     </div>
   )
 }
