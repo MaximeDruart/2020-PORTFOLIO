cd701b53836d8c979bb47f9a88603b45a3a0c991
Maxime Druart <maxime41010.druart@gmail.com>
2020-03-16 13:15:17 +0100

    Updates

---
+++ precache-manifest.1574feb2ce47f6a34757f4921ba6ce38.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ab4af5c12ba31544871139e0b4961a",
+    "url": "/2020-PORTFOLIO/index.html"
+  },
+  {
+    "revision": "df96a9011075066b577e",
+    "url": "/2020-PORTFOLIO/static/css/main.813281c8.chunk.css"
+  },
+  {
+    "revision": "361b54e6a305fd157090",
+    "url": "/2020-PORTFOLIO/static/js/2.01cc5a1c.chunk.js"
+  },
+  {
+    "revision": "5e243b52f22d6e97c6d7f0f00d7538bb",
+    "url": "/2020-PORTFOLIO/static/js/2.01cc5a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df96a9011075066b577e",
+    "url": "/2020-PORTFOLIO/static/js/main.54d009b4.chunk.js"
+  },
+  {
+    "revision": "970ce974237728b2783c",
+    "url": "/2020-PORTFOLIO/static/js/runtime-main.c8b6bdff.js"
+  },
+  {
+    "revision": "4b3c7e5143ad1995a76dc2fc881df6a5",
+    "url": "/2020-PORTFOLIO/static/media/DrukWideBold.4b3c7e51.woff"
+  },
+  {
+    "revision": "1cec4616687013848e928cf32fabae2b",
+    "url": "/2020-PORTFOLIO/static/media/coiny.1cec4616.jpg"
+  },
+  {
+    "revision": "b2d2656f49bf92f70a1411bb91e1382c",
+    "url": "/2020-PORTFOLIO/static/media/css-piano.b2d2656f.PNG"
+  },
+  {
+    "revision": "abaac753f68bee4eb719bbf49cde0136",
+    "url": "/2020-PORTFOLIO/static/media/helvetica.abaac753.otf"
+  },
+  {
+    "revision": "bf02a0a159b8787eb74b843675b1f1af",
+    "url": "/2020-PORTFOLIO/static/media/lab.bf02a0a1.PNG"
+  },
+  {
+    "revision": "09d168770915632028cb0d1e5657fa14",
+    "url": "/2020-PORTFOLIO/static/media/njs.09d16877.svg"
+  },
+  {
+    "revision": "e7f2a27370e3f4f8730afea21432f4ea",
+    "url": "/2020-PORTFOLIO/static/media/noise.e7f2a273.gif"
+  }
+]);
