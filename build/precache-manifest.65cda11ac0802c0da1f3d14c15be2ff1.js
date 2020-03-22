7a5f444196aebd069b8d57f1717b84ff99bdca60
Maxime Druart <maxime41010.druart@gmail.com>
2020-03-22 16:33:13 +0100

    setting build to public

---
+++ build/precache-manifest.65cda11ac0802c0da1f3d14c15be2ff1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e783564a66eb72be47f740d6c4223db9",
+    "url": "/2020-PORTFOLIO/index.html"
+  },
+  {
+    "revision": "4ee41cc67d0e6af76012",
+    "url": "/2020-PORTFOLIO/static/css/main.a90fb00c.chunk.css"
+  },
+  {
+    "revision": "564b5bd8de585bf7de06",
+    "url": "/2020-PORTFOLIO/static/js/2.42744bd9.chunk.js"
+  },
+  {
+    "revision": "d85bf2a906266485d98838f921f88637",
+    "url": "/2020-PORTFOLIO/static/js/2.42744bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee41cc67d0e6af76012",
+    "url": "/2020-PORTFOLIO/static/js/main.2df095f1.chunk.js"
+  },
+  {
+    "revision": "970ce974237728b2783c",
+    "url": "/2020-PORTFOLIO/static/js/runtime-main.c8b6bdff.js"
+  },
+  {
+    "revision": "4b3c7e5143ad1995a76dc2fc881df6a5",
+    "url": "/2020-PORTFOLIO/static/media/DrukWideBold.4b3c7e51.woff"
+  },
+  {
+    "revision": "e0b6c8ee547bf1363f1d86238def5f80",
+    "url": "/2020-PORTFOLIO/static/media/audio-player.e0b6c8ee.jpg"
+  },
+  {
+    "revision": "361ef11f48880f1dd53c6e61169b9ac9",
+    "url": "/2020-PORTFOLIO/static/media/coiny.361ef11f.jpg"
+  },
+  {
+    "revision": "4c5ef68f5aeba0f9a70c671d13f98c42",
+    "url": "/2020-PORTFOLIO/static/media/css-piano.4c5ef68f.jpg"
+  },
+  {
+    "revision": "aab73a7ce0b78341830e00345ea4a65d",
+    "url": "/2020-PORTFOLIO/static/media/etm-game.aab73a7c.jpg"
+  },
+  {
+    "revision": "e494ce44a4d8a09edead77fb7f0272c8",
+    "url": "/2020-PORTFOLIO/static/media/etm-menu.e494ce44.jpg"
+  },
+  {
+    "revision": "4a029241cb40add5d1ad3a17c5f273b9",
+    "url": "/2020-PORTFOLIO/static/media/flowfield.4a029241.JPG"
+  },
+  {
+    "revision": "abaac753f68bee4eb719bbf49cde0136",
+    "url": "/2020-PORTFOLIO/static/media/helvetica.abaac753.otf"
+  },
+  {
+    "revision": "0983285b8bafb87f78942da502e7628a",
+    "url": "/2020-PORTFOLIO/static/media/lab-game-2.0983285b.jpg"
+  },
+  {
+    "revision": "340db07e5170abdb772b68d582c63557",
+    "url": "/2020-PORTFOLIO/static/media/lab-game.340db07e.jpg"
+  },
+  {
+    "revision": "8fdc71e4fd19db0e3427fc80bd9a2190",
+    "url": "/2020-PORTFOLIO/static/media/lab-home.8fdc71e4.jpg"
+  },
+  {
+    "revision": "417fcda0945b1d45e2c487bac6427748",
+    "url": "/2020-PORTFOLIO/static/media/logo.417fcda0.jpg"
+  },
+  {
+    "revision": "6b356f23040da4c0bb68db92c6ea1791",
+    "url": "/2020-PORTFOLIO/static/media/njs-game.6b356f23.jpg"
+  },
+  {
+    "revision": "b94647fed485dbe11958781f087bd80c",
+    "url": "/2020-PORTFOLIO/static/media/njs-help.b94647fe.jpg"
+  },
+  {
+    "revision": "15b8f43848aeb461db02583379391262",
+    "url": "/2020-PORTFOLIO/static/media/njs-home-2.15b8f438.jpg"
+  },
+  {
+    "revision": "65637ebae81971658a6f6555b113d73f",
+    "url": "/2020-PORTFOLIO/static/media/njs-home.65637eba.jpg"
+  },
+  {
+    "revision": "a8b0a50bd876f9b8072c7cddd80a2b40",
+    "url": "/2020-PORTFOLIO/static/media/njs-levels.a8b0a50b.jpg"
+  },
+  {
+    "revision": "a9a3faa0d45c020d14cac1e4468ee129",
+    "url": "/2020-PORTFOLIO/static/media/njs-ninjas.a9a3faa0.jpg"
+  },
+  {
+    "revision": "09d168770915632028cb0d1e5657fa14",
+    "url": "/2020-PORTFOLIO/static/media/njs.09d16877.svg"
+  },
+  {
+    "revision": "e7f2a27370e3f4f8730afea21432f4ea",
+    "url": "/2020-PORTFOLIO/static/media/noise.e7f2a273.gif"
+  },
+  {
+    "revision": "8c3a935c981d6d0dcd40693b95a7b36f",
+    "url": "/2020-PORTFOLIO/static/media/pianoGif.8c3a935c.gif"
+  },
+  {
+    "revision": "34adad810f28ee9e6cbece1c68557aad",
+    "url": "/2020-PORTFOLIO/static/media/wiggly.34adad81.JPG"
+  }
+]);
